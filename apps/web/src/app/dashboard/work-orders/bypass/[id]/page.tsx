9050e8d6e9b1a21826219b0005e4cc4add675b8d
SansanSaga <154500077+SansanSaga@users.noreply.github.com>
2024-10-12 17:43:24 +0700

    fix: prod build (#26)

---
+++ apps/web/src/app/dashboard/work-orders/bypass/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardWorkOrdersBypassDetail = () => {
+  return (
+    <div>DashboardWorkOrdersBypassDetail</div>
+  )
+}
+
+export default DashboardWorkOrdersBypassDetail
